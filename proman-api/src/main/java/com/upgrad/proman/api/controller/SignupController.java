a93c04cd6900623737f936915168123c9b475c9a
Vikas Sinha <compuvikas@gmail.com>
2021-01-27 08:02:45 +0530

    PRO-004 : Added DB related items and application is booting up. proman-api is litening on port 8080. not able to see the swagger URL.

--- proman-api/src/main/java/com/upgrad/proman/api/controller/SignupController.java
+++ proman-api/src/main/java/com/upgrad/proman/api/controller/SignupController.java
@@ -2,6 +2,9 @@ package com.upgrad.proman.api.controller;
 
 import com.upgrad.proman.api.model.SignupUserRequest;
 import com.upgrad.proman.api.model.SignupUserResponse;
+import com.upgrad.proman.service.business.SignupBusinessService;
+import com.upgrad.proman.service.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,15 +12,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
 
 @RestController
 @RequestMapping("/")
 public class SignupController {
 
+    @Autowired
+    private SignupBusinessService signupBusinessService;
+
+
     @RequestMapping(method= RequestMethod.POST, path="/signup", consumes= MediaType.APPLICATION_JSON_UTF8_VALUE, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<SignupUserResponse> signup(final SignupUserRequest signupUserRequest)
     {
-        return new ResponseEntity(HttpStatus.OK);
+        final UserEntity userEntity = new UserEntity();
+        userEntity.setUuid(UUID.randomUUID().toString());
+        userEntity.setFirstName(signupUserRequest.getFirstName());
+        userEntity.setLastName(signupUserRequest.getLastName());
+        userEntity.setEmail(signupUserRequest.getEmailAddress());
+        userEntity.setPassword(signupUserRequest.getPassword());
+        userEntity.setSalt("12345hysgt");
+        userEntity.setStatus(4);
+        userEntity.setCreatedAt(ZonedDateTime.now());
+        userEntity.setCreatedBy("API BE");
+
+        final UserEntity createdUser = signupBusinessService.signup(userEntity);
+
+        SignupUserResponse userResponse = new SignupUserResponse().id(createdUser.getUuid()).status("REGISTERED");
+
+        return new ResponseEntity<SignupUserResponse>(userResponse,HttpStatus.CREATED);
     }
 }
 
