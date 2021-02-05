59e12a8c46471385678ed969310c87b1d40e210f
Vikas Sinha <compuvikas@gmail.com>
2021-02-06 00:42:33 +0530

    PRO-007 : Added support for JWT and encryption of password.

--- proman-api/src/main/java/com/upgrad/proman/api/exception/RestExceptionHandler.java
+++ proman-api/src/main/java/com/upgrad/proman/api/exception/RestExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.upgrad.proman.api.exception;
 
 import com.upgrad.proman.api.model.ErrorResponse;
+import com.upgrad.proman.service.exception.AuthenticationFailedException;
 import com.upgrad.proman.service.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,13 @@ import org.springframework.web.context.request.WebRequest;
 public class RestExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponse> resourceNotFoundException(ResourceNotFoundException exe, WebRequest request){
+        return new ResponseEntity<ErrorResponse>(
+                new ErrorResponse().code(exe.getCode()).message(exe.getErrorMessage()), HttpStatus.NOT_FOUND
+        );
+    }
+
+    @ExceptionHandler(AuthenticationFailedException.class)
+    public ResponseEntity<ErrorResponse> authenticationFailedException(AuthenticationFailedException exe, WebRequest request){
         return new ResponseEntity<ErrorResponse>(
                 new ErrorResponse().code(exe.getCode()).message(exe.getErrorMessage()), HttpStatus.NOT_FOUND
         );
--- proman-service/src/main/java/com/upgrad/proman/service/business/SignupBusinessService.java
+++ proman-service/src/main/java/com/upgrad/proman/service/business/SignupBusinessService.java
@@ -5,6 +5,7 @@ import com.upgrad.proman.service.dao.UserDao;
 import com.upgrad.proman.service.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class SignupBusinessService {
@@ -12,7 +13,17 @@ public class SignupBusinessService {
     @Autowired
     private UserDao userDao;
 
+    @Autowired
+    PasswordCryptographyProvider passwordCryptographyProvider;
+
+
+    @Transactional
     public UserEntity signup(UserEntity userEntity){
+
+        String[] encryptedText = passwordCryptographyProvider.encrypt(userEntity.getPassword());
+        userEntity.setSalt(encryptedText[0]);
+        userEntity.setPassword(encryptedText[1]);
+
         return userDao.createUser(userEntity);
     }
 }
